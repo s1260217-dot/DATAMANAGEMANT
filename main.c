db0b774f8248d09edae268b3eef5d88c94bcd51f
s1260217 <s1260217@u-aizu.ac.jp>
2019-07-05 12:14:27 +0900

    Won/lost message modified

--- main.c
+++ main.c
@@ -17,6 +17,6 @@ int main(){
   printf("Die 1: %d\n", die1);
   printf("Die 2: %d\n", die2);
   printf("Total value: %d\n", total);
-  if(total >= 7) printf("You won\n");
-  else printf("You lost");
+  if(total >= 7) printf("%s won\n",name);
+  else printf("%s lost\n", name);
 }
